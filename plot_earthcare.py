ccfdfb579202107e8acd457ba3543c7ef5a36213
Jesse Turner <jesseturner72@gmail.com>
2025-07-14 17:51:57 -0600

    Checks for differing inputs

--- plot_earthcare.py
+++ plot_earthcare.py
@@ -11,6 +11,14 @@ class EarthCarePlot:
         
         filename = os.path.basename(file_path)
 
+        if not os.path.isfile(file_path):
+            print("File not found.")
+            exit()
+
+        if not os.access(file_path, os.R_OK):
+            print("File found, but does not have read permissions.")
+            exit()            
+
         if "AC__TC__2B" in filename: 
             self.product_type = "AC__TC__2B"
             self.start_dt = self._get_dates(filename)
@@ -23,7 +31,7 @@ class EarthCarePlot:
                 self.longitude = np.array(f['ScienceData/longitude'][()])
 
         else: 
-            print("EarthCARE file type not added yet.")
+            print("Package not set up for this product yet.")
 
     
     def _get_dates(self, filename):
@@ -35,13 +43,21 @@ class EarthCarePlot:
             start_dt = datetime.datetime.strptime(start_str, "%Y%m%dT%H%M%S")
         
         return start_dt
+    
+
+    def info(self):
+        print(f"EarthCARE Product: {self.product_type}")
+        time = self._set_time()
+        print(f"Time: {datetime.datetime.strftime(min(time), '%Y-%m-%d %H:%M')} to {datetime.datetime.strftime(max(time), '%Y-%m-%d %H:%M')}")
+        print(f"Location: {round(self.latitude[0], 2)}, {round(self.longitude[0], 2)} to {round(self.latitude[-1], 2)}, {round(self.longitude[-1], 2)}")
+
 
     
     def plot(self, start_dt=None, end_dt=None):
 
-        filtered_time, filtered_data, filtered_height = self._filter_data(start_dt, end_dt)
+        time, data, height = self._filter_data(start_dt, end_dt)
 
-        time_grid, height_grid = np.meshgrid(filtered_time, np.arange(filtered_height.shape[1]), indexing='ij')
+        time_grid, height_grid = np.meshgrid(time, np.arange(height.shape[1]), indexing='ij')
 
         custom_colors = [
             '#c5c9c7', '#a2653e', '#ffffff', '#ff474c', '#0504aa', '#009337', '#840000',
@@ -57,7 +73,7 @@ class EarthCarePlot:
 
         cmap = ListedColormap(custom_colors)
 
-        pcm = ax.pcolormesh(time_grid, filtered_height, filtered_data, cmap=cmap, shading='auto')
+        pcm = ax.pcolormesh(time_grid, height, data, cmap=cmap, shading='auto')
 
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
         fig.autofmt_xdate()
@@ -68,24 +84,30 @@ class EarthCarePlot:
 
         output_dir = "plot_earthcare/"
         os.makedirs(output_dir, exist_ok=True)
-        fig.savefig(f"{output_dir}{self.product_type}_{datetime.datetime.strftime(self.start_dt, '%Y%m%d%H%M')}", dpi=200, bbox_inches='tight')
+        save_path = f"{output_dir}{self.product_type}_{datetime.datetime.strftime(self.start_dt, '%Y%m%d%H%M')}"
+        fig.savefig(save_path, dpi=200, bbox_inches='tight')
+        print(f"Plot saved at {save_path}.")
 
 
     def _filter_data(self, start_dt=None, end_dt=None):
         if start_dt and end_dt:
                 self.start_dt, self.end_date = start_dt, end_dt
         else:
-            print("Using full {self.product_type} swath.")
+            print(f"Using full {self.product_type} swath.")
         
         height_km = self.height_data/1000 # convert to km
         time = self._set_time()
 
         if start_dt is not None and end_dt is not None:
             time_mask = (time >= start_dt) & (time <= end_dt)
-
             filtered_time = time[time_mask]
             filtered_data = self.class_data[time_mask, :]
             filtered_height = height_km[time_mask, :]
+
+        else: 
+            filtered_time = time
+            filtered_data = self.class_data
+            filtered_height = height_km
 
         return filtered_time, filtered_data, filtered_height
     
@@ -107,14 +129,9 @@ class EarthCarePlot:
 
         ax.plot(abbr_lon, abbr_lat, color='red', marker='o', linewidth=1, transform=ccrs.PlateCarree())
 
-        #extent = [-80, -25, 30, 60]
-        #ax.set_extent(extent, crs=ccrs.PlateCarree())
-
-        # Annotate each point with time (HH:MM), within the visible extent
         for lon, lat, t in zip(abbr_lon, abbr_lat, abbr_time):
-            #if extent[0] <= lon <= extent[1] and extent[2] <= lat <= extent[3]:
-                time_label = t.strftime('%H:%M')
-                ax.text(lon + 0.5, lat, time_label, fontsize=8, transform=ccrs.PlateCarree())
+            time_label = t.strftime('%H:%M')
+            ax.text(lon + 0.5, lat, time_label, fontsize=8, transform=ccrs.PlateCarree())
 
         ax.coastlines()
         ax.add_feature(cfeature.BORDERS, linestyle=':')
@@ -130,10 +147,12 @@ class EarthCarePlot:
 
         output_dir = "plot_earthcare/"
         os.makedirs(output_dir, exist_ok=True)
-        fig.savefig(f"{output_dir}{self.product_type}_{datetime.datetime.strftime(self.start_dt, '%Y%m%d%H%M')}_ground_track", dpi=200, bbox_inches='tight')
+        save_path = f"{output_dir}{self.product_type}_{datetime.datetime.strftime(self.start_dt, '%Y%m%d%H%M')}_ground_track"
+        fig.savefig(save_path, dpi=200, bbox_inches='tight')
+        print(f"Plot saved at {save_path}.")
 
 
 
 ec = EarthCarePlot("data_AC__TC__2B/ECA_EXAD_AC__TC__2B_20250703T062938Z_20250704T000519Z_06229A.h5")
-#ec.plot(datetime.datetime(2025, 7, 3, 6, 21), datetime.datetime(2025, 7, 3, 6, 24))
 ec.plot_groundtrack()
+#ec.info()
